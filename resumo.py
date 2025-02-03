5913462e6deae1d23b1c8280e2434a4ce9e9b7f6
Devisree <143163502+devi1262005@users.noreply.github.com>
2025-02-03 17:38:52 +0530

    Update resumo.py

--- resumo.py
+++ resumo.py
@@ -226,4 +226,4 @@ def extract_text_from_file(file_path):
         raise ValueError("Unsupported file type")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
